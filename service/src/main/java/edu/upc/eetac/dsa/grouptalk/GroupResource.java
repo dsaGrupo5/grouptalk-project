5c0963be5aa8e59b5186da82e90e1a2d82aeeb63
jtardio <jesus.tardio.lopez@gmail.com>
2015-11-19 10:07:46 +0100

    metodo eliminar grupo funcionando

--- service/src/main/java/edu/upc/eetac/dsa/grouptalk/GroupResource.java
+++ service/src/main/java/edu/upc/eetac/dsa/grouptalk/GroupResource.java
@@ -119,20 +119,23 @@ public class GroupResource {
         return colecciongrupos;
     }
 
-    @Path("/eliminar")
+    @RolesAllowed({"administrador"})
+    @Path("/eliminar/{nombre}")
     @DELETE
-    public void eliminarTema(@FormParam("nombregrupo") String nombre) throws GrupoNoExisteException {
+    public Response eliminarGrupo(@PathParam("nombre") String nombregrupo) throws GrupoNoExisteException {
 
-        String grupoid = securityContext.getUserPrincipal().getName();
-        temaDAO temadao = new temaDAOImpl();
-        try {
-            String idusercreador = temadao.obtener_tema_por_id(id).getUserid();
-            if(!temaid.equals(idusercreador))
-                throw new ForbiddenException("este usuario no ha creado este tema!");
-            if(!temadao.eliminar_tema(id))
-                throw new TemaIDNoExisteException();
-        } catch (SQLException e) {
+        GrupoDAO grupoDAO = new GrupoDAOImpl();
+
+        Grupo grupo = null;
+
+        try{
+            if(!grupoDAO.eliminar_grupo(nombregrupo))
+                throw new GrupoNoExisteException();
+        }catch (GrupoNoExisteException e){
+
+        }catch(SQLException e){
             throw new InternalServerErrorException();
         }
+        return Response.ok().build();
     }
 }
